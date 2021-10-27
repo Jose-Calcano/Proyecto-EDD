939c00f169b527556bdafc9c0ed6adc1760f8fbe
Juanixter <j27790605@correo.unimet.edu.ve>
2021-10-27 00:55:01 -0400

    Dijkstra listo

--- src/Classes/GrafoMA.java
+++ src/Classes/GrafoMA.java
@@ -1,5 +1,7 @@
 package Classes;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author juanc
@@ -18,6 +20,24 @@ public class GrafoMA {
 
     public boolean isEmpty() {
         return numVertices == 0;
+    }
+
+    public void añadirVertice(int i, int j, int value) {
+        if (matrizAdy[i][j] != 0) {
+            matrizAdy[i][j] = value;
+        } else {
+            matrizAdy[i][j] = value;
+            numVertices += 1;
+        }
+    }
+
+    public void eliminarVertice(int i, int j) {
+        if (matrizAdy[i][j] != 0) {
+            matrizAdy[i][j] = 0;
+            numVertices -= 1;
+        } else {
+            JOptionPane.showMessageDialog(null, "No hay un vertice en esta posición.");
+        }
     }
 
     public String rutasString(ListaClientes clientes, ListaRestaurantes restaurantes) {
@@ -64,20 +84,53 @@ public class GrafoMA {
 
     }
 
-    public void rutaMasCortaDijkstra(Pedido pedido, ListaClientes clientes, ListaRestaurantes restaurantes) {
-        int destinoIndex = Integer.parseInt(pedido.llegada) - 1;
-        int origenIndex = clientes.size - 1 + restaurantes.getNumOfRestaurant(pedido.salida.charAt(0));
-        String ruta = "";
-        boolean flag = true;
-        int dist = 0;
-        for (int i = 0; i < maxNodos; i++) {
-            int prueba = this.matrizAdy[origenIndex][i];
-            if (prueba > 0 && flag) {
-                dist = prueba;
-                flag = false;
-            } else if (prueba > 0 && prueba < dist) {
-                dist = prueba;
+    public int getMinimumVertex(boolean[] revisados, int[] key) {
+        int minKey = Integer.MAX_VALUE;
+        int vertex = -1;
+        for (int i = 0; i < this.maxNodos; i++) {
+            if (revisados[i] == false && minKey > key[i]) {
+                minKey = key[i];
+                vertex = i;
             }
         }
+        return vertex;
+    }
+
+    public int[] rutaMasCortaDijkstra(int origenIndex) {
+        boolean[] spt = new boolean[this.maxNodos];
+        int[] distance = new int[this.maxNodos];
+        int inf = Integer.MAX_VALUE;
+        for (int i = 0; i < this.maxNodos; i++) {
+            distance[i] = inf;
+        }
+        distance[origenIndex] = 0;
+        for (int i = 0; i < this.maxNodos; i++) {
+            int vert1 = this.getMinimumVertex(spt, distance);
+            spt[vert1] = true;
+            for (int vert2 = 0; vert2 < this.maxNodos; vert2++) {
+                if (this.matrizAdy[vert1][vert2] > 0) {
+                    if (spt[vert2] == false && this.matrizAdy[vert1][vert2] != inf) {
+                        int newKey = matrizAdy[vert1][vert2] + distance[vert1];
+                        if (newKey < distance[vert2]) {
+                            distance[vert2] = newKey;
+                        }
+                    }
+                }
+            }
+        }
+        return distance;
+    }
+
+    public static void main(String[] args) {
+        GrafoMA grafo = new GrafoMA(5);
+        grafo.añadirVertice(0, 1, 2);
+        grafo.añadirVertice(2, 4, 4);
+        grafo.añadirVertice(0, 2, 3);
+        grafo.añadirVertice(1, 4, 22);
+        int[] rutas = grafo.rutaMasCortaDijkstra(0);
+        for (int i = 0; i < rutas.length; i++) {
+            System.out.println(rutas[i]);
+        }
+
     }
 }
