87954abefdeea0c8128eb290b72342f924a8252c
Jose-Calcano <josefcarevalo@gmail.com>
2021-10-30 22:04:22 -0400

    A

--- src/Classes/Funciones.java
+++ src/Classes/Funciones.java
@@ -20,6 +20,9 @@ public class Funciones {
 
     String line;
     String clientes_txt;
+    ListaClientes ListaDeClientes = new ListaClientes();
+    ListaRestaurantes ListaDeRestaurantes = new ListaRestaurantes();
+    ListaPedidos ListaDePedidos = new ListaPedidos();
 
     public Funciones() {
 
@@ -63,7 +66,7 @@ public class Funciones {
                 jf.showOpenDialog(null);
                 File archivo = jf.getSelectedFile();
                 String ruta = archivo.getAbsolutePath();
-                try (PrintWriter pw = new PrintWriter(ruta)) {
+                try ( PrintWriter pw = new PrintWriter(ruta)) {
                     pw.print(newTxt);
                 }
                 JOptionPane.showMessageDialog(null, "Actualización exitosa.");
@@ -82,7 +85,7 @@ public class Funciones {
         ImageIcon scaledIcon = new ImageIcon(imgScale);
         frame.setIcon(scaledIcon);
     }
-    
+
     public void lecturaArchivo(String rutaCarga) {
         try {
             File file = new File(rutaCarga);
@@ -95,27 +98,31 @@ public class Funciones {
                 }
             }
         } catch (Exception e) {
-   
+
         }
         String[] splitRestaurantes = clientes_txt.split("Clientes");
-    
+
         String[] splitPedidos = splitRestaurantes[1].split("Pedidos");
-    
+
         String[] splitRutas = splitPedidos[1].split("Rutas");
-    
+
         String[] splitFinal = splitRestaurantes[0].split("Restaurantes");
-    
-        System.out.println(splitFinal[1]); //Restaurante
-                        
+
+        /*System.out.println(splitFinal[1]); //Restaurante
+
         System.out.println(splitPedidos[0]); //Clientes
 
         System.out.println(splitRutas[0]); //Pedidos
 
-        System.out.println(splitRutas[1]); //Rutas
-        
+        System.out.println(splitRutas[1]); //Rutas*/
+
         Funciones test = new Funciones();
-        
+
         test.crearClientes(splitPedidos[0]);
+        System.out.println("\n");
+        test.crearRestaurantes(splitFinal[1]);
+        System.out.println("\n");
+        test.crearPedidos(splitRutas[0]);
 
     }
 
@@ -123,17 +130,135 @@ public class Funciones {
         Funciones contar = new Funciones();
         int clientesIteracion = contar.countLines(clientes);
         String print = clientes.replace(",", "\n");
-        System.out.println(print);
-        for (int i = 0; i <= clientesIteracion; i++) {
-        }
-            try ( BufferedReader bufReader = new BufferedReader(new StringReader(print))) {
-                String line;
-                while ((line = bufReader.readLine()) != null) {
-                    System.out.println(line);
-                    System.out.println("Prueba");
+        //System.out.println(print);
+        //for (int i = 0; i <= clientesIteracion; i++) {
+         //   String e = String.valueOf(i);
+            print = print.replace(".", "");
+       // }
+        try ( BufferedReader bufReader = new BufferedReader(new StringReader(print))) {
+            String line;
+            int contador = 0;
+            int parametro1 = 0;
+            int contador2 = 0;
+            String parametro2 = "";
+            String parametro3 = "";
+            String parametro4 = "";
+         
+            
+            while ((line = bufReader.readLine()) != null) {
+                if(contador == 1){
+                     parametro1 = Integer.parseInt(line);
+                     System.out.println(parametro1);
                 }
-            } catch (Exception e) {
+                if(contador != 0 && contador == 2){
+                    parametro2 = line;
+                     System.out.println(parametro2);
+                }
+                if(contador != 0 && contador == 3){
+                    parametro3 = line;
+                    System.out.println(parametro3);
+                }
+                if(contador != 0 && contador == 4){
+                    ++contador2;
+                    parametro4 = line;
+                    Cliente nuevoCliente = new Cliente(parametro1, parametro2, parametro3, parametro4);
+                    ListaDeClientes.addAtTheEnd(nuevoCliente);
+                    contador = 0;
+                    System.out.println(parametro4);
+                }
+              //  System.out.println(contador + ") " +  line);
+                ++contador;
             }
+        } catch (Exception e) {
+        }
+    }
+    
+        public void crearRestaurantes(String restaurantes) {
+        Funciones contarRestaurantes = new Funciones();
+        int RestauranteIteracion = contarRestaurantes.countLines(restaurantes);
+        String printRestaurantes = restaurantes.replace(",", "\n");
+            System.out.println(printRestaurantes);
+        try ( BufferedReader bufReaderRestuarante = new BufferedReader(new StringReader(printRestaurantes))) {
+            String lineR;
+            int contador = 0;
+            String parametro1 = "";
+            int contador2 = 0;
+            String parametro2 = "";
+            String parametro3 = "";
+            String parametro4 = "";
+         
+            
+            while ((lineR = bufReaderRestuarante.readLine()) != null) {
+                
+                if(contador == 1){
+                     parametro1 = lineR;
+                     System.out.println(parametro1);
+                }
+                if(contador != 0 && contador == 2){
+                    parametro2 = lineR;
+                     System.out.println(parametro2);
+                }
+                if(contador != 0 && contador == 3){
+                    ++contador2;
+                    parametro4 = lineR;
+                    Restaurante nuevoRestaurante = new Restaurante(parametro1.charAt(0), parametro2, parametro3);
+                    ListaDeRestaurantes.addAtTheEnd(nuevoRestaurante);
+                    contador = 0;
+                    System.out.println(parametro4);
+                }
+              //  System.out.println(contador + ") " +  line);
+                ++contador;
+                ++contador2;
+                if(contador2 == RestauranteIteracion){
+                break;
+                }
+            }
+        } catch (Exception e) {
+        }
+    }
+        
+            public void crearPedidos(String pedidos) {
+        String printPedidos = pedidos.replace(",", "\n");
+        System.out.println(printPedidos);
+        Funciones contarPedidos = new Funciones();
+        int pedidosIteracion = contarPedidos.countLines(pedidos);
+        try ( BufferedReader bufReaderRestuarante = new BufferedReader(new StringReader(printPedidos))) {
+            String lineR;
+            int contador = 0;
+            String parametro1 = "";
+            int contador2 = 0;
+            String parametro2 = "";
+            String parametro3 = "";
+            String parametro4 = "";
+         
+            
+            while ((lineR = bufReaderRestuarante.readLine()) != null) {
+                if(contador2 == pedidosIteracion){
+                break;
+                }
+                if(contador == 1){
+                     parametro1 = lineR;
+                     System.out.println(parametro1);
+                }
+                if(contador != 0 && contador == 2){
+                    parametro2 = lineR;
+                     System.out.println(parametro2);
+                }
+
+                if(contador != 0 && contador == 3){
+                    ++contador2;
+                    parametro4 = lineR;
+                    Pedido nuevoPedido = new Pedido(parametro1, parametro2, parametro3);
+                    ListaDePedidos.addAtTheEnd(nuevoPedido);
+                    contador = 0;
+                    System.out.println(parametro4);
+                }
+              //  System.out.println(contador + ") " +  line);
+                ++contador;
+                ++contador2;
+            }
+        } catch (Exception e) {
+        }
     }
 
     public int countLines(String str) {
